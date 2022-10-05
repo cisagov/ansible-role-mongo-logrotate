c38ebd0c09f284850cdb1474479da7607a75ada3
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-10-05 17:59:06 -0400

    Update molecule tests
    Update the Python tests in our molecule testing to pass pre-commit and
    to be in line with our other Ansible role projects.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,20 +12,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize('pkg', [
-    'logrotate'
-])
-def test_packages(host, pkg):
-    package = host.package(pkg)
-
-    assert package.is_installed
+@pytest.mark.parametrize("pkg", ["logrotate"])
+def test_apt_packages(host, pkg):
+    """Test that the apt packages were installed."""
+    assert host.package(pkg).is_installed
 
 
-@pytest.mark.parametrize('file,content', [
-    ('/etc/logrotate.d/mongo', '^/var/log/mongodb')
-])
-def test_files(host, file, content):
-    f = host.file(file)
-
-    assert f.exists
-    assert f.contains(content)
+@pytest.mark.parametrize(
+    "file",
+    [
+        {
+            "contents": "^/var/log/mongodb",
+            "mode": "0o644",
+            "path": "/etc/logrotate.d/mongo",
+        }
+    ],
+)
+def test_files(host, file):
+    """Test that appropriate files exist."""
+    assert host.file(file["path"]).exists
+    assert host.file(file["path"]).is_file
+    assert oct(host.file(file["path"]).mode) == file["mode"]
+    assert host.file(file["path"]).contains(file["contents"])
